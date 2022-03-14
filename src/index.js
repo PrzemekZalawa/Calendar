34f6bb50a9c9c1e9128ea006f984d3cf92cabde4
PrzemekZalawa <84127988+PrzemekZalawa@users.noreply.github.com>
2022-03-14 10:53:12 +0100

    wynoszenie stanu w gore
    JESZCZE nei dziala B)

--- src/index.js
+++ src/index.js
@@ -31,78 +31,18 @@ const months =[
   {id:11,name:"listopad",startFrom:4,dayAmount:30},
   {id:12,name:"grudzien",startFrom:4,dayAmount:30}
 ]
-
-let monthName;
-/*let siemaEniu;
- let displayMonth;
-function DaysOfMonth(){
-let [displayMonth,setDisplayMonth] = useState();
-let papadżi = (months.map((day, i)=>{
-  if(day.id==siemaEniu)
-  {
- for(let x=0; x<day.startFrom;x++){
-   return(
-     <div className='day'> 
-
-     </div>
-   )
- }
- for(let j=day.startFrom; j<day.dayAmount;j++){
-   return(
-     <div className='day'>
-         {j}
-     </div>
-   )
- }
- for(let y=day.dayAmount; y<35-day.dayAmount;y++){
-   return(
-     <div className='day'>
-
-     </div>
-   )
- }
-}   
-}))
-let len;
-console.log(papadżi.length)
-console.log(papadżi)
-len = papadżi.length;
-
-for(let i = 0; i < len; i++ ){
-  papadżi[i] && papadżi.push(papadżi[i]);  
-}
-papadżi.splice(0 , len);  
-setDisplayMonth(papadżi);
-console.table(displayMonth)
-return(
-  <tr>
-  <td>
-    {displayMonth}
-  </td>
-</tr>
-)
-}
-*/
+  
 const week= daysOfWeek.map((day, i)=>{
   return(
   <th key={day+i}>{day.name}</th>
   )
 })
-function GetMonthName(){
-  let [selectedMonth,selectMonth]=useState();
-  selectMonth(monthName)
-  return(
-    
-    <th colSpan={7}>{selectedMonth}</th>
-  )
-  
-}
+
 class MonthTable extends react.Component{
   constructor(props) {
     super(props);
     // this.handleClick = this.handleClick.bind(this)
     this.state={
-      // test: displayMonth
       
     }
   }
@@ -110,7 +50,7 @@ class MonthTable extends react.Component{
       return (
          <table>
            <tr>
-             <GetMonthName/>
+             <th colSpan={7}>miesiac</th>
            </tr>
            <tr>
              {week}
@@ -120,79 +60,25 @@ class MonthTable extends react.Component{
       )
     }
   }  
-  
-
-
-
 class MonthButton extends react.Component{
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
+    // this.handleClick = this.handleClick.bind(this)
     this.state={
-      // test :[[]]
+      name: ''
       
     }
   }  
-
-
-   handleClick(monthID){
-    monthName = months[monthID].name;
-    
-    //  siemaEniu= monthID
-    //    displayMonth= months.map((day, i)=>{
-    //      if(day.id==monthID)
-    //      {
-    //     for(let x=0; x<day.startFrom;x++){
-    //       return(
-    //         <div className='day'> 
-  
-    //         </div>
-    //       )
-    //     }
-    //     for(let j=day.startFrom; j<day.dayAmount;j++){
-    //       return(
-    //         <div className='day'>
-    //             {j}
-    //         </div>
-    //       )
-    //     }
-    //     for(let y=day.dayAmount; y<35-day.dayAmount;y++){
-    //       return(
-    //         <div className='day'>
-  
-    //         </div>
-    //       )
-    //     }
-      
-       
-    // }   
-       
-    // })
-    // let len;
-    //   len = displayMonth.length;
-  
-    //   for(let i = 0; i < len; i++ ){
-    //     displayMonth[i] && displayMonth.push(displayMonth[i]);  
-    //   }
-    //   displayMonth.splice(0 , len);  
-  
-    //   console.table(displayMonth)
-    //   console.log(displayMonth)
-
-    //   this.setState({
-    //     test: displayMonth
-    //   })
-       
-    //   console.table(this.state.displayMonth)
-    //   console.log(this.state.displayMonth)
-  
-    
+  handleClick(name){
+    this.props.name = name;
   }
-  render(){
-    // let [displayMonth,setDisplayMonth] = useState();
+
     
+  
+  render(){
+    const monthName = this.props.name
     return(
-      <td><button onClick={()=>this.handleClick(this.props.monthID)}>{this.props.name}</button></td>
+      <td><button onClick={()=>this.handleClick(this.props.name)}>{this.props.name}</button></td>
     )
   }
 }
@@ -201,19 +87,26 @@ const year = months.map((row,i)=>{
     return(
       <tr key={i}>
         <MonthButton   dayAmount={months[i].dayAmount}
-        name={months[i].name} startFrom={months[i].startFrom} monthID={months[i].id}/>      
+        name={months[i].name} startFrom={months[i].startFrom} monthID={months[i].id} test={this.getName}/>      
                 <MonthButton  dayAmount={months[i+1].dayAmount}
-        name={months[i+1].name} startFrom={months[i+1].startFrom} monthID={months[i+1].id}/>      
+        name={months[i+1].name} startFrom={months[i+1].startFrom} monthID={months[i+1].id} test={this.getName}/>      
                 <MonthButton  dayAmount={months[i+2].dayAmount}
-        name={months[i+2].name} startFrom={months[i+2].startFrom} monthID={months[i+2].id}/>      
+        name={months[i+2].name} startFrom={months[i+2].startFrom} monthID={months[i+2].id} test={this.getName}/>      
                 <MonthButton  dayAmount={months[i+3].dayAmount}
-        name={months[i+3].name} startFrom={months[i+3].startFrom} monthID={months[i+3].id} />       
+        name={months[i+3].name} startFrom={months[i+3].startFrom} monthID={months[i+3].id} test={this.getName}/>       
       </tr>
     )
   }
 })
 
 class Calendar extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {name: ''};
+  }
+  getName(monthName){
+    this.setState({name: monthName})
+  }
   render(){
     return(
       <div>
